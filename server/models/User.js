2a00b75882b5a80e54931f7a17a40119161ab2c1
ayoitshasya <hasyasahithya@gmail.com>
2025-06-15 16:28:19 +0530

    google auth

--- server/models/User.js
+++ server/models/User.js
@@ -1,53 +1,82 @@
 import mongoose, { Schema } from "mongoose";
+import passportLocalMongoose from "passport-local-mongoose";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
+    googleId: {
+      type: String,
+      default: null,
+    },
+    profilePicture: {
+      type: String,
+      default: null,
+    },
+    resetPasswordToken: {
+      type: String,
+      default: null,
+    },
+    resetPasswordExpires: {
+      type: Date,
+      default: null,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
     fname: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     lname: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     e_id: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique:true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: false, // Marked as not required since Google users may not have a password
     },
     department: {
-        type:String,
-        enum: ['COMPS', 'IT', 'EXTC', 'SAH', 'EXCP', 'MECH'],
-        required: true
+      type: String,
+      enum: ['COMPS', 'IT', 'EXTC', 'SAH', 'EXCP', 'MECH'],
+      required: true,
     },
-    designation:{
-        type: String,
-        required: true
+    designation: {
+      type: String,
+      required: true,
     },
-    date_of_appointment:{
-        type: Date,
-        required: true
+    date_of_appointment: {
+      type: Date,
+      required: true,
     },
-    present_appointment:{
-        type: Date,
-        required: true
+    present_appointment: {
+      type: Date,
+      required: true,
     },
-    user_type:{
-        type: String,
-        enum:['employee', 'hod', 'principal', 'fdc-conveyor', 'fdc-coordinator'],
-        required:true
-    }
+    user_type: {
+      type: String,
+      enum: ['employee', 'hod', 'principal', 'fdc-conveyor', 'fdc-coordinator'],
+      required: true,
+    },
+  },
+  {
+    timestamps: { createdAt: true, updatedAt: false },
+  }
+);
 
-    
-}, {timestamps: { createdAt: true, updatedAt: false }})
+// Enable username/password login support if needed
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: 'email', // use email as the login field
+  usernameLowerCase: true,
+});
 
-
-export const userModel = mongoose.model('User', userSchema)
+export const userModel = mongoose.model('User', userSchema);
