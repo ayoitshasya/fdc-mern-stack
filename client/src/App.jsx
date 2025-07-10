96a198d852008c80a016f833d5845c0833571e0b
ayoitshasya <hasyasahithya@gmail.com>
2025-07-10 08:50:52 +0530

    added routes but please check thanks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
 import Application1 from './pages/Application/Application1'
+import Application2 from './pages/Application/Application2'
+import Application3 from './pages/Application/Application3'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -13,14 +15,14 @@ function App() {
     <div className='h-screen flex flex-col'>
       <div  className='h-6 bg-[#B7202E] w-full z-10 top-0'></div>
       <div className='bg-[url(/campus.jpg)] bg-cover w-screen flex-1'>
-        <Routes>
+        { <Routes>
           <Route path="/" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/application" element={<Application1 />} />
-          {/* Add more routes as needed */}
-        </Routes>
-
+          <Route path="/application" element={<Application2 />} />
+          <Route path="/application" element={<Application3 />} />
+        </Routes> }
       </div>
     </div>
       
