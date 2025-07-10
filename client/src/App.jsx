9c4260d37d7ca2b5b237ba50a52793d4e4918cb1
ayoitshasya <hasyasahithya@gmail.com>
2025-07-10 10:53:14 +0530

    added application status ka routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
 import Application1 from './pages/Application/Application1'
 import Application2 from './pages/Application/Application2'
 import Application3 from './pages/Application/Application3'
-import { Routes, Route } from 'react-router-dom'
+import ApplicationStatus from './pages/Application/ApplicationStatus'
 
 function App() {
 
@@ -19,9 +20,10 @@ function App() {
           <Route path="/" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/application" element={<Application1 />} />
-          <Route path="/application" element={<Application2 />} />
-          <Route path="/application" element={<Application3 />} />
+          <Route path="/application/1" element={<Application1 />} />
+          <Route path="/application/2" element={<Application2 />} />
+          <Route path="/application/3" element={<Application3 />} />
+          <Route path="/application/Status" element={<ApplicationStatus />} />
         </Routes> }
       </div>
     </div>
