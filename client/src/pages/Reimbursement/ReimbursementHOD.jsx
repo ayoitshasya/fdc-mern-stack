a5766081869c7984efef582328ee43c3f88c7f48
ayoitshasya <hasyasahithya@gmail.com>
2025-07-17 18:09:17 +0530

    deleted application fdc convenor page, added reimbursement hod page, renamed application and reimbursement principal pages to fdc

--- client/src/pages/Reimbursement/ReimbursementHOD.jsx
+++ client/src/pages/Reimbursement/ReimbursementHOD.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Header from '../../Components/Header'
 
-function ApplicationHOD() {
+function ReimbursementHOD() {
   return (
     <div className='w-full h-full flex flex-col'>
       <Header/>
       <div className='w-full h-full flex flex-col justify-center items-center'>
         <div className='bg-[#FAFAFA] rounded-3xl p-4 px-8 flex flex-col items-center font-inter'>
-          <h1 className='text-xl text-[#3D3D3D] font-medium mb-4'>Recommendation from FDC Convenor</h1>
+          <h1 className='text-xl text-[#3D3D3D] font-medium mb-4'>Recommendation from HOD</h1>
           <form className='w-full text-[#7F7F7F] font-normal flex flex-col'>
             <label htmlFor="recommended" className=''>Recommended/Non-Recommended: *</label>
             <select name="recommended" className='w-full border rounded-lg p-1 outline-none mt-1 mb-2' defaultValue="">
@@ -18,14 +18,7 @@ function ApplicationHOD() {
 
             <div className='flex items-center justify-between mb-4'>
                 <label htmlFor="checkedLoadAdjustment" className='text-[#3D3D3D] text-sm md:text-base mt-2 mb-1'>
-                I have checked the Load Adjustment Form *
-                </label>
-                <input type="checkbox" name="checkedLoadAdjustment" id="checkedLoadAdjustment" className='accent-[#B7202E] w-5 h-5'/>
-            </div>
-
-            <div className='flex items-center justify-between mb-4'>
-                <label htmlFor="checkedLoadAdjustment" className='text-[#3D3D3D] text-sm md:text-base mt-0 mb-1'>
-                I have checked brochure *
+                I have checked all the information *
                 </label>
                 <input type="checkbox" name="checkedLoadAdjustment" id="checkedLoadAdjustment" className='accent-[#B7202E] w-5 h-5'/>
             </div>
@@ -33,9 +26,6 @@ function ApplicationHOD() {
             <label htmlFor="reason">Reason for Recommendation/Non-Recommendation *</label>
             <input type="text" name='reason' className='w-full border rounded-lg p-1 outline-none mt-1 mb-2'/>
             
-            <label htmlFor="meetingDate">Date of Meeting: *</label>
-            <input type="date" name="meetingDate" className='w-full border rounded-lg p-1 outline-none mt-1 mb-4 bg-white'/>
-
             <p className='font-medium text-[#666666]'>Check above details and click submit</p>
             <div className='flex items-center mb-4'>
                 <span className='font-light'>I have checked the above details</span>
@@ -50,4 +40,4 @@ function ApplicationHOD() {
   )
 }
 
-export default ApplicationHOD
+export default ReimbursementHOD
