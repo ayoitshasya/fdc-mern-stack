dde050f0644dd16dee82312c216d3e7088d88087
Ashish Dhiwar <dhiwarashish4@gmail.com>
2025-08-21 21:21:32 +0530

    logo now redirects to home page

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -40,7 +40,7 @@ function Header() {
   return (
     <div className="w-full bg-white py-2 px-8 flex justify-between items-center sticky z-10 top-0">
       <div className="h-full flex items-center gap-8">
-        <img src="/mini-logo.png" className="h-14" />
+        <img src="/mini-logo.png" className="h-14 cursor-pointer" onClick={()=> navigate("/")} />
         <h1 className="font-inter font-light text-lg">
           Welcome, {profileUser.fname} {profileUser.lname}
         </h1>
